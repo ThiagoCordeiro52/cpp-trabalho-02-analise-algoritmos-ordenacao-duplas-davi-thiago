0be21f0b282043d28eb4c231e61454fa8323b90b
davi-i <josedavifrancelino@gmail.com>
2021-07-20 19:54:41 -0300

    Removed unecessary code

--- source/main.cpp
+++ source/main.cpp
@@ -58,7 +58,7 @@ void print_diff( const std::chrono::time_point<std::chrono::steady_clock> &start
 /// The running options
 struct RunningOpt{
     size_t min_sample_sz{1000};  //!< Default 10^5.
-    size_t max_sample_sz{10000}; //!< The max sample size.
+    size_t max_sample_sz{50000}; //!< The max sample size.
     int n_samples{25};           //!< The number of samples to collect.
 
     /// Returns the sample size step, based on the [min,max] sample sizes and # of samples.
@@ -268,23 +268,15 @@ constexpr short N_RUNS = 5;
 //=== The main function, entry point.
 int main( int argc, char * argv[] ){
     // Process any command line arguments.
-    // TODO: create proper cli for this program.
-    // sort_suit [running_options] <nominal_alg_list>
-    // $./sort_suite -min 10000 -max 100000 -s 25 quick insertion radix
     RunningOpt run_opt;
     DataSet dataset{run_opt};
     
-    std::ofstream errors;
-    errors.open("errors.txt");
-    errors << "DATATYPE\tSIZE\tALGO" << std::endl;
-
     // FOR EACH DATA SCENARIO DO...
     while (not dataset.has_ended()){
         // Collect data in a linear (arithmetic) scale.
         // FOR EACH SAMPLE SIZE DO...
         std::ofstream out_file;
         out_file.open(dataset.to_string() + ".txt");
-        std::cout << ">>> Starting " << dataset.to_string() << '\n';
         for (auto ns{0}; ns < run_opt.n_samples; ns++) {
             // FOR EACH SORTING ALGORITHM DO...
             // Select the first sorting algorithm.
@@ -297,21 +289,15 @@ int main( int argc, char * argv[] ){
             std::vector<int> backup;
             backup.resize(size);
 
-            // temp vector to check to correcteness of each algo
-            std::vector<int> temp;
-            temp.resize(size);
-            std::copy(dataset.begin_data(), dataset.end_data(), temp.begin());
-            std::sort(temp.begin(), temp.end());
-
             AlgorithmCollection algorithms{};
             std::ostringstream line;
             line << size;
             std::cout << dataset.to_string() << ":\t>>> Size: " << size << '\n';
             while (not algorithms.has_ended()) {
-                // Run each algorithm N_RUN times and calculate a sample avarage for each dependent variable.
-                // FOR EACH RUN DO...This is necessary to reduce any measurement noise.
                 std::cout << "\t\t>>> Running " << algorithms.to_string() << "...\n";
                 double elapsed_time_mean = 0;
+                // Run each algorithm N_RUN times and calculate a sample avarage for each dependent variable.
+                // FOR EACH RUN DO...This is necessary to reduce any measurement noise.
                 for (auto ct_run(0); ct_run < N_RUNS; ++ct_run) {
                     std::copy(dataset.begin_data(), dataset.end_data(), backup.begin());
                     // Reset timer
@@ -330,32 +316,25 @@ int main( int argc, char * argv[] ){
                     elapsed_time_mean += (std::chrono::duration <double, std::milli> (diff).count() - elapsed_time_mean) / static_cast<double>(ct_run + 1);
                 } // Loop all runs for a single sample size.
                 // Check if the algorithm gava the right result
-                if (backup != temp) {
-                    errors << dataset.to_string() << '\t' << size << '\t' << algorithms.to_string() << std::endl;
-                    errors << "\t\tGiven:    " << std::vector<int>(dataset.begin_data(), dataset.end_data());
-                    errors << "\t\tExpected: " << temp;
-                    errors << "\t\tReceived: " << backup;
-                }
                 line << '\t' << std::setw(9) << elapsed_time_mean;
+                // Printing header
                 if (ns == 0)
                     out_file << '\t' << std::setw(9) << algorithms.to_string();
                 algorithms.next();
-            } // Loop through all algorithms activated.
+            }
             // DATA COLLECTION FOR THIS SAMPLE SIZE (ROW) ENDS HERE.
             // If this is the first time, we must first print the header.
             // Send out data line to the output file.
             if (ns == 0)
                 out_file << std::endl;
             out_file << line.str() << std::endl;
-        } // Loop through each sample size required.
+        }
         // Go to the next active scenario.
         dataset.next();
         dataset.generate_data();
         // Close the file corresponding to this dataset.
         out_file.close();
-    } // Loop data scenarios.
-
-    errors.close();
+    }
 
     return EXIT_SUCCESS;
 }
